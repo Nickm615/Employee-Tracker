30803a44856d623c0cc3a815925d4c76aa7bf238
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-10-24 21:04:17 -0500

    First switch case is working, displays department table with console.table

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO department (name)
+VALUES ('Logistics'),
+('sales'),
+('Human Resources')
--- index.js
+++ index.js
@@ -1,49 +1,5 @@
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user:'root',
-        password:'root',
-        database:'employee_db'
-    },
-    console.log('Connected to the employee_db databse')
-);
-const question = [
-    {
-        type: 'list',
-        message:'What would you like to do?',
-        choices:['View all departments','View all roles','View all employees','Add a department','Add a role','Add an employee','Update an employee role'],
-        name:'choice'
-    }
-]
+const init = require('./initPrompt')
 
-function init() {
-    inquirer
-    .prompt(question)
-    .then(response => {
-        switch (response.choice) {
-            case 'View all departments':
-                
-                break;
-            case 'View all roles':
-                
-                break;
-            case 'View all employees':
-                
-                break;
-            case 'Add a department':
-                
-                break;
-            case 'Add an employee':
-                
-                break;
-            case 'Update an employee role':
-                
-                break;
-            case 'Add a role':
-                
-                break;
-        }
-    })
-}
+init();
+
+
